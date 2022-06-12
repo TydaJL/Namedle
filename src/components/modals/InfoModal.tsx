ffafbce4a5c8ab3767ed4a32a2c43c752981b94d
Tyler Lam <81004062+TydaJL@users.noreply.github.com>
2022-06-11 23:29:34 -0700

    Personalizing the infobox
    Personalizing the infobox

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -18,54 +18,74 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="T"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
         <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="L" />
         <Cell value="E" />
+        <Cell value="R" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter T is in the name and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="D" />
+        <Cell 
+        isRevealing={true}
+        isCompleted={true}
+        value="A"
+        status="present"
+        />
+        <Cell value="V" />
+        <Cell value="I" />
+        <Cell value="D" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter A is in the name but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="J" />
+        <Cell value="A" />
+        <Cell value="M" />
+        <Cell isRevealing={true} isCompleted={true} value="E" status="absent" />
+        <Cell value="S" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter E is not in the name in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is an open source version of the word guessing game we all know and
-        love -{' '}
+        love.
+        <br></br>
+        <br></br>
+        Code of Namedle:
+        {' '}
+        <a
+          href="https://github.com/TydaJL/Namedle"
+          className="underline font-bold"
+        >
+          Check out the code here!
+        </a>{' '}
+
+        <br></br>
+        <br></br>
+        Forked from 
+        {' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          React Wordle
         </a>{' '}
+
+        <br></br>
+        <br></br>
+
+        Created by Tyler Lam
       </p>
     </BaseModal>
   )
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,7 +1,7 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const GAME_COPIED_MESSAGE = 'Game copied to clipboard!'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const HARD_MODE_ALERT_MESSAGE =
@@ -21,9 +21,9 @@ export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
 export const NEW_WORD_TEXT = 'New word in'
 export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const TOTAL_TRIES_TEXT = 'Total Tries'
+export const SUCCESS_RATE_TEXT = 'Success Rate'
+export const CURRENT_STREAK_TEXT = 'Current Streak'
+export const BEST_STREAK_TEXT = 'Best Streak'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
   "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
